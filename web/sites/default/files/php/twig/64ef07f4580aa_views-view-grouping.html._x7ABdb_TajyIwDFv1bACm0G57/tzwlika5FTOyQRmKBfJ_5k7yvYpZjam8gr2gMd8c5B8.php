9f579c529243856a283758a892eba8c3df0237d4
vhelperv <vladyslav.p014@gmail.com>
2023-08-30 19:25:53 +0300

    base 08

---
+++ web/sites/default/files/php/twig/64ef07f4580aa_views-view-grouping.html._x7ABdb_TajyIwDFv1bACm0G57/tzwlika5FTOyQRmKBfJ_5k7yvYpZjam8gr2gMd8c5B8.php
@@ -0,0 +1,99 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/claro/templates/classy/views/views-view-grouping.html.twig */
+class __TwigTemplate_6c48d3a08b560a7c7af6c25731d8e0b6 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 17
+        echo "<div class=\"view-grouping\">
+  <div class=\"view-grouping-header\">";
+        // line 18
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 18, $this->source), "html", null, true);
+        echo "</div>
+  <div class=\"view-grouping-content\">";
+        // line 19
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 19, $this->source), "html", null, true);
+        echo "</div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/claro/templates/classy/views/views-view-grouping.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  46 => 19,  42 => 18,  39 => 17,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/claro/templates/classy/views/views-view-grouping.html.twig", "/var/www/web/core/themes/claro/templates/classy/views/views-view-grouping.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 18);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
